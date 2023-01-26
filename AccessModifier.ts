862c120a99a5ea6655e4ebf58bca141c9717f000
RubaiRahman <rubairahman1@gmail.com>
2023-01-27 00:06:49 +0600

    AccessModifier

---
+++ AccessModifier.ts
@@ -0,0 +1,28 @@
+class Cricketer {
+  name: string;
+  private age: number;
+  readonly country: string;
+
+  constructor(n: string, a: number, c: string) {
+    this.name = n;
+    this.age = a;
+    this.country = c;
+  }
+  play() {
+    console.log(`${this.name} from ${this.country} is Playing`);
+  }
+}
+
+//As we can see we can use class as data type in ts this will make sure that only with that specific class object will be valid to the array .otherwise it will show Error
+
+const Tamim = new Cricketer("Tamim", 40, "Bangladesh");
+const Musfiq = new Cricketer("Musfiq", 38, "Bangladesh");
+const crickters: Cricketer[] = [];
+crickters.push(Tamim);
+crickters.push(Musfiq);
+
+//As we see the class properties which is declared as private will not be accessible outside of the class. And readOnly can be read but could't modify it. By default properties of the class is declared as public.
+Tamim.age = "Iqbal"
+Tamim.country = "england"
+console.log(Tamim.age)
+console.log(Tamim.country)
